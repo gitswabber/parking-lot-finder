dcafe9e9a9491d89fe67700d625084dcfd1d470f
Chie <doutorking@gmail.com>
2020-02-06 20:29:49 +0900

    Add properties files of test

--- backend-module/src/test/java/com/swabber/api/service/seoul/api/ParkingLotStorageJobIntegrationTest.java
+++ backend-module/src/test/java/com/swabber/api/service/seoul/api/ParkingLotStorageJobIntegrationTest.java
@@ -5,17 +5,9 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
 import java.util.Date;
 
