fd33f0b77b09659a777185fd07352797801a7196
Chie <doutorking@gmail.com>
2020-02-11 01:17:25 +0900

    Working on tc for batch.

--- backend-module/src/test/java/com/parking/lot/api/batch/ParkingLotStorageJobIntegrationTest.java
+++ backend-module/src/test/java/com/parking/lot/api/batch/ParkingLotStorageJobIntegrationTest.java
@@ -1,30 +1,47 @@
 package com.parking.lot.api.batch;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
 @ActiveProfiles("test")
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class ParkingLotStorageJobIntegrationTest {
+
     @Autowired
-    private JobLauncher jobLauncher;
+    private JobRepository jobRepository;
 
     @Autowired
     private Job parkingLotStorageJob;
 
     @Test
-    public void parkingLotStorageJob() throws Exception {
-        final JobParameters jobParameters = new JobParametersBuilder().addDate("date", new Date()).toJobParameters();
-        jobLauncher.run(parkingLotStorageJob, jobParameters);
+    void parkingLotStorageJob() throws Exception {
+        final SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
+        simpleJobLauncher.setJobRepository(jobRepository);
+        simpleJobLauncher.afterPropertiesSet();
+        final JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+        jobLauncherTestUtils.setJobLauncher(simpleJobLauncher);
+        jobLauncherTestUtils.setJobRepository(jobRepository);
+        jobLauncherTestUtils.setJob(parkingLotStorageJob);
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(new JobParametersBuilder().addDate("date", new Date()).toJobParameters());
+        JobInstance actualJobInstance = jobExecution.getJobInstance();
+
+        Assertions.assertEquals(actualJobInstance.getJobName(), "parkingLotStorageJob");
     }
 }
